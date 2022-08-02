5c8eb58296b21e150d02d7cca188466b544c16dc
hedgehog1979 <egor.pub@yandex.ru>
2022-08-02 20:48:01 +0400

    rnd

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double NumberA = new Random().Next(1,10);
+Console.WriteLine(NumberA);
+double NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+Console.WriteLine(NumberA/NumberB);
