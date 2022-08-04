5a1db752806df6980ee82834cdac8d2ea3653658
hedgehog1979 <egor.pub@yandex.ru>
2022-08-04 22:24:41 +0400

    2022_08_04_22_24

--- Seminar1 Task1/Program.cs
+++ Seminar1 Task1/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите первое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int numB = Convert.ToInt32(Console.ReadLine());
-if (numA = numB * numB)
+if (numA == numB * numB)
 {
 Console.WriteLine("да");
 }
