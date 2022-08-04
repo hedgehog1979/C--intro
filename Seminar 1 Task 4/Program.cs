795c74c1b3d1fad2a9dfa93f7c0a5caf76cc2e6a
hedgehog1979 <egor.pub@yandex.ru>
2022-08-04 23:54:17 +0400

    Seminar1 2022_08_04_fin

--- Seminar 1 Task 4/Program.cs
+++ Seminar 1 Task 4/Program.cs
@@ -1,4 +1,11 @@
 ﻿Console.WriteLine("Введите трехзначное сило и мы покажем Вам последнюю цифру");
 int num = Convert.ToInt32(Console.ReadLine());
-int rem = num%10;
-Console.WriteLine(rem);
+if (num>99 && num<1000)
+{
+    Console.WriteLine(num%10);
+}
+else
+{ 
+    Console.Write("не то число");
+}
+
